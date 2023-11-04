334e89c9e376fdf9e54d692f806e749b13de8511
NEERAJ-PANDEY <neerajpandey007star@gmail.com>
2023-11-04 17:02:30 +0530

    ass7cw

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Footer from './component/Footer';
+import Navbar from './component/Navbar';
+import Body from './component/body';
+import Quotes from './component/quotes.json';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      {
+        Quotes.quote.map((ele)=>(
+          <Body text={ele.quote} author = {ele.author}></Body>
+        ))
+
+      }
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
